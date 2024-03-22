35eb19194658405243caaef03aa8d5f7ef62cf6e
Sajjadahmedshaikh <160344749+Sajjadahmedshaikh@users.noreply.github.com>
2024-03-21 18:31:44 -0700

    Add files via upload

---
+++ homework.ts
@@ -0,0 +1,21 @@
+// let a: number = 5;
+// let b : number = 2;
+// //let c : number = 
+// // 5+1 =6 + 6+1= 7 + 2-1=1 1-1 =0 + 7 + 0+1 =1 +1=23
+// let c= ++a + a + a++ + --b + a + b++ +b;
+
+// console.log(c)
+
+// let weightinkg= 80;
+// let heightinmeters = 2.75;
+// // bmi formula= weighinkg / (hightinmeters * hightinmeters )
+// let bmi = 80 /(2.75 * 2.75)
+
+// console.log(`your BMI is ${bmi}`);
+
+let wightinkg = 100;
+let hightinmeters = 5.80
+
+let bmi = 100 /(5.80*5.80)
+
+console.log(`your BMI is ${bmi}`)
---
+++ main.ts
@@ -0,0 +1,77 @@
+//let firstName ="sajjad";
+//let age = 23;
+
+//let message = `hello my name is:${firstName} and my age is ${age}:`
+
+//console.log(message)
+
+
+// let index= 10;
+// ++index
+// ++index
+// ++index
+// ++index
+// ++index
+
+// console.log (index)
+
+
+//urinary operators
+//let index =10;
+//              10+1    11+1    + 7 + 12-1 -2 
+
+// let result = ++index + ++index + 7 +
+// --index -2
+
+// console.log(result)
+
+
+// assignment operators
+// let num1 =15;
+// num1 +=10;
+// num1 -=5;
+
+// console.log(num1)
+
+// COMPARISON OPERATORS 
+// = = THESE ARE THE COMPARISON OPERATORS
+// ! notequls two 
+// < >
+
+
+
+// let num1 = 5
+// let num2 =3
+
+// let result = num1 = num2
+
+// console.log (result)
+
+// let num1 = 5
+// let num2 = 3
+
+// let result = num1 != num2 
+
+// console.log (result)
+
+
+// let num1 = 55
+// let num2 = 55
+
+// let result = num1 > num2
+
+// console.log(result)
+
+
+// let age = 18
+
+// let result = age >= 18
+
+// console.log(result)
+
+
+let age = 18
+
+let result = age <= 18
+
+console.log(result)
